da2c130d1e7e5399042c673539561ef6a7e093ce
Cristian Ortega <crisortegamunoz@gmail.com>
2023-08-13 18:06:09 -0400

    Fuentes proyecto generado desde spring initializr

---
+++ src/main/java/com/cristianortega/escalabproject/EscalabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cristianortega.escalabproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalabProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalabProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cristianortega/escalabproject/EscalabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianortega.escalabproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscalabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
